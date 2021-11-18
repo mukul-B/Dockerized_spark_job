ba7fb17b299fd67a099cce1ad0b57eb42374e1e8
muku <mukulbadhan@gmail.com>
2021-11-17 20:40:22 -0800

    linear regression prediction

--- m2.py
+++ m2.py
@@ -1,17 +1,40 @@
-from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.feature import FeatureHasher
 from pyspark.ml.regression import LinearRegression
-from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
-from pyspark.sql import SparkSession, Window, Row
-from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType
 
-spark = SparkSession.builder.master("local[1]")\
-                            .appName("SparkByExamples.com")\
-                            .getOrCreate()
+spark = SparkSession.builder.master("local[1]") \
+    .appName("SparkByExamples.com") \
+    .getOrCreate()
+
+SCHEMA = StructType([StructField('fixed acidity', DoubleType()),
+                     StructField('volatile acidity', DoubleType()),
+                     StructField('citric acid', DoubleType()),
+                     StructField('residual sugar', DoubleType()),
+                     StructField('chlorides', DoubleType()),
+                     StructField('free sulfur dioxide', IntegerType()),
+                     StructField('total sulfur dioxide', IntegerType()),
+                     StructField('density', DoubleType()),
+                     StructField('pH', DoubleType()),
+                     StructField('sulphates', DoubleType()),
+                     StructField('alcohol', DoubleType()),
+                     StructField('quality', IntegerType())])
 # Prepare training and test data.
-data = spark.read.option("header",True).option("delimiter",";")\
+data = spark.read.schema(SCHEMA).option("header", True).option("delimiter", ";") \
     .csv("winequality-white.csv")
-train, test = data.randomSplit([0.8, 0.2], seed=12345)
-print(test.count())
-print(train.count())
-train.select(col('fixed acidity')).show()
 
+hasher = FeatureHasher(inputCols=[c for c in data.columns if c not in {'quality'}],
+                       outputCol="features")
+featurized = hasher.transform(data)
+# featurized.show(truncate=False)
+train, test = featurized.randomSplit([0.8, 0.2], seed=12345)
+
+lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8).setLabelCol("quality")
+
+# Fit the model
+lrModel = lr.fit(train)
+print("Coefficients: %s" % str(lrModel.coefficients))
+print("Intercept: %s" % str(lrModel.intercept))
+
+predictions = lrModel.transform(test)
+predictions.select("prediction", "quality").show(5)
