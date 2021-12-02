096ef9e2a327ffc4668e1f32256f73cb53fbe003
muku <mukulbadhan@gmail.com>
2021-12-02 02:01:49 -0800

    issue in random forest regression

--- m2.py
+++ m2.py
@@ -5,10 +5,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import round
 from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import VectorIndexer
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import RegressionEvaluator
 
 
 spark = SparkSession.builder.master("local[1]") \
     .appName("cch2") \
+    .config("spark.driver.memory", "15g")\
     .getOrCreate()
 
 SCHEMA = StructType([StructField('fixed acidity', DoubleType()),
@@ -29,28 +33,33 @@ data = spark.read.schema(SCHEMA).option("header", True).option("delimiter", ";")
 
 hasher = FeatureHasher(inputCols=[c for c in data.columns if c not in {'quality'}],
                        outputCol="features")
-featurized = hasher.transform(data)
+featurized = hasher.transform(data).select("features","quality")
 # featurized.show(truncate=False)
+# exit(0)
+featureIndexer =VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=10).fit(featurized)
+
 train, test = featurized.randomSplit([0.8, 0.2], seed=12345)
 
-lr = LinearRegression(maxIter=20, regParam=0.3, elasticNetParam=0.8).setLabelCol("quality")
-lrModel = lr.fit(train)
+rf = RandomForestRegressor(featuresCol="indexedFeatures").setLabelCol("quality")
 
-# lr = RandomForestRegressor(numTrees=2, maxDepth=2)
-# lrModel = lr.fit(train)
 
-# print("Coefficients: %s" % str(lrModel.coefficients))
-# print("Intercept: %s" % str(lrModel.intercept))
+# Chain indexer and forest in a Pipeline
+pipeline = Pipeline(stages=[featureIndexer, rf])
 
-predictions = lrModel.transform(test)
-predictionAndLabels=predictions.withColumn("prediction", round(predictions["prediction"]).cast(DoubleType()))\
-    .select("prediction", "quality")
-predictionAndLabels.show(5)
+rfModel = pipeline.fit(train)
 
-evaluatorMulti = MulticlassClassificationEvaluator(labelCol="quality", predictionCol="prediction")
+# Make predictions.
+predictions = rfModel.transform(test)
+# Select example rows to display.
+predictions.select("prediction", "label", "features").show(5)
 
-f1 = evaluatorMulti.evaluate(predictionAndLabels, {evaluatorMulti.metricName: "f1"})
+# Select (prediction, true label) and compute test error
+evaluator = RegressionEvaluator(
+    labelCol="label", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
 
-print(f1)
+rfModel = rfModel.stages[1]
+print(rfModel)  # summary only
 
 
